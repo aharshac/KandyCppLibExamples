24c2d66bf88afdbf53a7ccd4e010f2660a8223f8
Harsha <Harsha>
2017-05-03 19:30:48 +0530

    Added global vars

--- src/sms_test.cpp
+++ src/sms_test.cpp
@@ -4,27 +4,39 @@
 using namespace std;
 using namespace kandy;
 
+
+/* Kandy authentication data */
+string domain_api_key = "";
+string domain_api_secret = "";
+string user_id = "";
+
+/* SMS data */
+string source = "";		// SMS sender, phone number in international format, i.e., +12345678
+string destination = "";	// SMS recipient, phone number in international format, i.e., +12345678
+string message = "";
+
+
 int main() {
-	try {
-		string user_access_token, device_id;
+    try {
+	string user_access_token, device_id;	// Kandy auth result, passed by reference on success
 
-		cout << "Kandy C++ SMS Library" << endl;
-		cout << "Initialising SMS Sender" << endl;
+	cout << "Kandy C++ SMS Library" << endl;
+	cout << "Initialising SMS Sender" << endl;
 
-       	sms sender = sms("<domain_api_key>", "<domain_api_secret>", "<user_id>", user_access_token, device_id);
-		
-		if (user_access_token.empty() or device_id.empty()) {
-			cout << "Could not initialise SMS Sender" << endl;
-			return 1;
-		}
+	sms sender = sms(domain_api_key, domain_api_secret, user_id, user_access_token, device_id);
+	
+	if (user_access_token.empty() or device_id.empty()) {
+		cout << "Could not initialise SMS Sender" << endl;
+		return 1;
+	}
 
-		cout << "Initialiisation Success" << endl;
-		cout << "User Access Token: " + user_access_token << endl;
-		cout << "Device ID: " + device_id << endl;
+	cout << "Initialiisation Success" << endl;
+	cout << "User Access Token: " + user_access_token << endl;
+	cout << "Device ID: " + device_id << endl;
 
 
-		bool sent = sender.send("<source>", "<destination>", "<message>");
-		cout << (sent ? "SMS sent successfully!" : "SMS not sent!") << endl;
+	bool sent = sender.send(source, destination, message);
+	cout << (sent ? "SMS sent successfully!" : "SMS not sent!") << endl;
     } catch (exception &e) {
     	cout << e.what() << endl;
     }    
